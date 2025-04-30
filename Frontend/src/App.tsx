22b1959e0347cfb8eb665553f09257ae952b831d
LuciusL <lucius.lechner@porsche.digital>
2025-04-30 10:14:59 +0200

    Jetzt mit backend glaub ich

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import netroLogo from "./assets/netro.png";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
@@ -9,35 +9,48 @@ function App() {
     return (
         <>
             <div>
-                <a
-                    href="https://vite.dev"
-                    target="_blank"
-                >
-                    <img
-                        src={viteLogo}
-                        className="logo"
-                        alt="Vite logo"
-                    />
+                <header>
+                    <nav>
+                        <h1>
+                            <a href="/">
+                                <i className="fa-sharp fa-solid fa-dice-d20"></i>{" "}
+                                Netro
+                            </a>
+                        </h1>
+                        <ul>
+                            <li>
+                                <a href="/">
+                                    <i className="fa-regular fa-circle-user"></i>{" "}
+                                    Sign In
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-                <a
-                    href="https://react.dev"
-                    target="_blank"
-                >
+                <a href="https://react.dev" target="_blank">
                     <img
-                        src={reactLogo}
+                        src={netroLogo}
                         className="logo react"
                         alt="React logo"
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>Netro</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
             </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
         </>
     );
 }
